356d41077bf4b8b70023329cdd17e6fc2efca52e
Ugwuoke Richard <ugwuokerichard5859@gmail.com>
2022-10-13 15:39:29 -0700

    t6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program.
+ * Returns: returns 0.
+ */
+int main(void)
+{
+	printf("size of char: %d\n", sizeof(char));
+	printf("size of int: %d\n", sizeof(int));
+	printf("size of long int: %d\n", sizeof(long int));
+	printf("size of long long int: %d\n", sizeof(long long int));
+	printf("size of float: %d\n" sizeof(float));
+	return (0);
+}
