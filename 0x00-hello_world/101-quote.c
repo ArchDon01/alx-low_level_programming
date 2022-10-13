6d24068d864a8a700cb7604383ebe60fbee07f25
Ugwuoke Richard <ugwuokerichard5859@gmail.com>
2022-10-13 16:44:05 -0700

    last

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: return zero.
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\"-Dora Kopar, 2015-10-19\n",59);
+        return (1);
+}
