cb2043561a2f6ab9c73a6d97a235e569c31c042d
Ugwuoke Richard <ugwuokerichard5859@gmail.com>
2022-10-13 15:46:03 -0700

    t6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ int main(void)
 	printf("size of int: %d\n", sizeof(int));
 	printf("size of long int: %d\n", sizeof(long int));
 	printf("size of long long int: %d\n", sizeof(long long int));
-	printf("size of float: %d\n" sizeof(float));
+	printf("size of float: %d\n", sizeof(float));
 	return (0);
 }
