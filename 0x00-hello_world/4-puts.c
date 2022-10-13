df713180c9703833a8db6171686191de9c240195
Ugwuoke Richard <ugwuokerichard5859@gmail.com>
2022-10-13 14:56:00 -0700

    t4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main- Serves as the enntry point for the program
- * Return: By default returns zero if no errors otherwise returns 
- * non zero value.
+ * main - Serves as the enntry point for the program
+ * Return: This returns 0
  */
 int main(void)
 {
