45cbc16083fd9efc16db6ac6d75af05bb36686b0
Ugwuoke Richard <ugwuokerichard5859@gmail.com>
2022-10-13 15:59:25 -0700

    t6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("size of char: %d byte(S)\n", sizeof(char));
-	printf("size of int: %d byte(S)\n", sizeof(int));
-	printf("size of long int: %d byte(S)\n", sizeof(long int));
-	printf("size of long long int: %d byte(S)\n", sizeof(long long int));
-	printf("size of float: %d byte(S)\n", sizeof(float));
+	printf("size of char: %ld byte(S)\n", sizeof(char));
+	printf("size of int: %ld byte(S)\n", sizeof(int));
+	printf("size of long int: %ld byte(S)\n", sizeof(long int));
+	printf("size of long long int: %ld byte(S)\n", sizeof(long long int));
+	printf("size of float: %ld byte(S)\n", sizeof(float));
 	return (0);
 }
