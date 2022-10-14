a5ef752c7126aafd781d2d5be602102e040c0166
Ugwuoke Richard <ugwuokerichard5859@gmail.com>
2022-10-14 11:38:35 -0700

    start

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,21 +10,17 @@ int main(void)
 {
 	int n;
 
-	printf("n ");
-	scanf("%d", &n);
-	printf("n %d\n", n);
-
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("is zero\n");
+		printf("is zero\n", n);
 	}
 	else
 	{
-		printf("is negative\n");
+		printf("is negative\n", n);
 	}
 
 	return (0);
