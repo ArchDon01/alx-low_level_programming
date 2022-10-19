d2e19404be4a6bd7f50a378491637ef1e3d6af55
Ugwuoke Richard <ugwuokerichard5859@gmail.com>
2022-10-18 17:29:30 -0700

    task

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -14,6 +15,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
 
 #endif
